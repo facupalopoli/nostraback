e4d6894332fb4e84e5f42eb170272b6ee1f1bc86
juanchitho <jbauti9@gmail.com>
2023-07-10 14:45:45 -0300

    tratando de agregar destacados

--- public/js/featured.js
+++ public/js/featured.js
@@ -1,5 +1,62 @@
-"use strict"
+// $(document).ready(function() {
+//     var cardContainer = $('.divProducts1');
+//     var cardWidth = $('.cardProduct').outerWidth(true);
+//     var cardsToShow = 2;
+//     var scrollAmount = cardWidth * cardsToShow;
+  
+      
+//       $(window).on('resize', function() {
+//         if ($(window).width() < 780) {
+//           cardsToShow = 2;
+//           scrollAmount = (cardWidth + 30) * cardsToShow;
+//       } else {
+//           cardsToShow = 4;
+//           scrollAmount = (cardWidth + 17) * cardsToShow;
+//           cardContainer.css('transform', '');
+//       }
+//       }).resize();
+  
+//       $('.next-btn').click(function() {
+//       cardContainer.animate({ scrollLeft: '+=' + scrollAmount }, 'slow');
+//       });
+  
+//       $('.prev-btn').click(function() {
+//       cardContainer.animate({ scrollLeft: '-=' + scrollAmount }, 'slow');
+//       });
+//   });
 
-const btnDer = document.querySelector('.btnDer')
-const btnIzq = document.querySelector('.btnIzq')
-let currentSlide = 0
+document.addEventListener('DOMContentLoaded', () => {
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const productContainer = document.querySelector('.product-container');
+
+  let scrollAmount = 0;
+  const productWidth = 300; // Adjust this value as per your product card width
+  const visibleProducts = 3;
+  const totalProducts = document.querySelectorAll('.cardProduct').length;
+  const totalGroups = Math.ceil(totalProducts / visibleProducts);
+
+  nextBtn.addEventListener('click', () => {
+      scrollAmount += productWidth * visibleProducts;
+      productContainer.style.transform = `translateX(-${scrollAmount}px)`;
+
+      if (scrollAmount >= (totalGroups - 1) * productWidth * visibleProducts) {
+          nextBtn.disabled = true;
+      }
+
+      prevBtn.disabled = false;
+  });
+
+  prevBtn.addEventListener('click', () => {
+      scrollAmount -= productWidth * visibleProducts;
+      productContainer.style.transform = `translateX(-${scrollAmount}px)`;
+
+      if (scrollAmount === 0) {
+          prevBtn.disabled = true;
+      }
+
+      nextBtn.disabled = false;
+  });
+});
+
+  
