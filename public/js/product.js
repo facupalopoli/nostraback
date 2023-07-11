e72284900b41104d4984d7dccd276ec796c77af4
juanchitho <jbauti9@gmail.com>
2023-07-11 19:29:22 -0300

    navbarProductos pero le falta

--- public/js/product.js
+++ public/js/product.js
@@ -1,3 +1,5 @@
+// juan js
+
 function selectSize(button) {
     
     var buttons = document.getElementsByClassName("size-btn");
@@ -60,6 +62,42 @@ $(document).ready(function() {
 
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//   const menuToggle = document.querySelector('.menu-toggle');
+//   const menu = document.querySelector('.menu');
+
+//   menuToggle.addEventListener('click', function() {
+//     menu.classList.toggle('active');
+    
+//   });
+
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const dropdowns = document.querySelectorAll('.dropdown');
+
+//   dropdowns.forEach(function(dropdown) {
+//     const trigger = dropdown.querySelector('a');
+//     const menu = dropdown.querySelector('.dropdown-menu');
+
+//     trigger.addEventListener('click', function(e) {
+//       e.preventDefault();
+//       dropdowns.forEach(function(dropdown) {
+//         dropdown.classList.remove('active');
+        
+//       });
+//       dropdown.classList.add('active');
+//     });
+
+//     document.addEventListener('click', function(e) {
+//       if (!dropdown.contains(e.target)) {
+//         dropdown.classList.remove('active');
+//       }
+//     });
+//   });
+// });
+
+/* funcion menu hamburguesa */
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.menu-toggle');
   const menu = document.querySelector('.menu');
@@ -70,7 +108,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 });
-
+/* funcion para abrir la seccion de shop */
 document.addEventListener('DOMContentLoaded', function() {
   const dropdowns = document.querySelectorAll('.dropdown');
 
@@ -94,5 +132,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+/* funcionalidad del menu logueado */
+document.addEventListener('DOMContentLoaded', function() {
+  const dropdowns = document.querySelectorAll('.dropdown-logueado');
 
+  dropdowns.forEach(function(dropdown) {
+    const trigger = dropdown.querySelector('a');
+    const menu = dropdown.querySelector('.dropdown-menu-logueado');
 
+    trigger.addEventListener('click', function(e) {
+      e.preventDefault();
+      dropdowns.forEach(function(dropdown) {
+        dropdown.classList.remove('active');
+        
+      });
+      dropdown.classList.add('active');
+    });
+
+    document.addEventListener('click', function(e) {
+      if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove('active');
+      }
+    });
+  });
+});
