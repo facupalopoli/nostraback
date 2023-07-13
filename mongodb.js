0014bf04da37b3f66c05d34c921bea28706ccb8f
juanchitho <jbauti9@gmail.com>
2023-07-13 12:38:31 -0300

    todo el responsive de productos.ejs

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose
-/* .connect('mongodb://127.0.0.1:27017/nostraGuille') */
- .connect('mongodb+srv://tpfinalmongo:h3FlPOOR7ltruacL@tpfinaldb.g0hzp2c.mongodb.net/nostraDB')  
+.connect('mongodb://127.0.0.1:27017/nostraGuille')
+//  .connect('mongodb+srv://tpfinalmongo:h3FlPOOR7ltruacL@tpfinaldb.g0hzp2c.mongodb.net/nostraDB')  
 .then(()=>console.log('la db esta conectada'))
 .catch(error=>console.log(error))
