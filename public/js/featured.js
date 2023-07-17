3fdeb4edfdfc7cbaa3f67405ab63ccc18daa119c
juanchitho <jbauti9@gmail.com>
2023-07-16 22:15:12 -0300

    responsive productos destacados y correccion de errores

--- public/js/featured.js
+++ public/js/featured.js
@@ -1,62 +1,93 @@
-// $(document).ready(function() {
-//     var cardContainer = $('.divProducts1');
-//     var cardWidth = $('.cardProduct').outerWidth(true);
-//     var cardsToShow = 2;
-//     var scrollAmount = cardWidth * cardsToShow;
-  
-      
-//       $(window).on('resize', function() {
-//         if ($(window).width() < 780) {
-//           cardsToShow = 2;
-//           scrollAmount = (cardWidth + 30) * cardsToShow;
-//       } else {
-//           cardsToShow = 4;
-//           scrollAmount = (cardWidth + 17) * cardsToShow;
-//           cardContainer.css('transform', '');
-//       }
-//       }).resize();
-  
-//       $('.next-btn').click(function() {
-//       cardContainer.animate({ scrollLeft: '+=' + scrollAmount }, 'slow');
-//       });
-  
-//       $('.prev-btn').click(function() {
-//       cardContainer.animate({ scrollLeft: '-=' + scrollAmount }, 'slow');
-//       });
-//   });
 
+
+  
 document.addEventListener('DOMContentLoaded', () => {
-  const prevBtn = document.querySelector('.prev-btn1');
-  const nextBtn = document.querySelector('.next-btn1');
-  const productContainer = document.querySelector('.product-container');
+    const prevBtn = document.querySelector('.prev-btn1');
+    const nextBtn = document.querySelector('.next-btn1');
+    const productContainer = document.querySelector('.product-container');
 
-  let scrollAmount = 0;
-  const productWidth = 400; // Adjust this value as per your product card width
-  const visibleProducts = 3;
-  const totalProducts = document.querySelectorAll('.cardProduct1').length;
-  const totalGroups = Math.ceil(totalProducts / visibleProducts);
+    let scrollAmount = 0;
+    const desktopProductWidth = 400; // Adjust this value as per your product card width in desktop view
+    const tabletProductWidth = 300; // Adjust this value as per your product card width in tablet view
+    const phoneProductWidth = 215; // Adjust this value as per your product card width in phone view
+    const gap = 15; // Adjust this value to match the gap between product cards
+    let visibleProducts = 3; // Initial value for desktop view
 
-  nextBtn.addEventListener('click', () => {
-      scrollAmount += (productWidth+15) * (visibleProducts);
-      productContainer.style.transform = `translateX(-${scrollAmount}px)`;
+    function updateVisibleProducts() {
+        if (window.innerWidth >= 1024) {
+            visibleProducts = 3; // Desktop view
+        } else if (window.innerWidth >= 768) {
+            visibleProducts = 2; // Tablet view
+        } else {
+            visibleProducts = 1; // Phone view
+        }
+    }
 
-      if (scrollAmount >= (totalGroups - 1) * productWidth * visibleProducts) {
-          nextBtn.disabled = true;
-      }
+    function updateScrollAmount() {
+        let productWidth;
 
-      prevBtn.disabled = false;
-  });
+        if (visibleProducts === 3) {
+            productWidth = desktopProductWidth;
+        } else if (visibleProducts === 2) {
+            productWidth = tabletProductWidth;
+        } else {
+            productWidth = phoneProductWidth;
+        }
 
-  prevBtn.addEventListener('click', () => {
-      scrollAmount -= (productWidth+15) * visibleProducts;
-      productContainer.style.transform = `translateX(-${scrollAmount}px)`;
+        const totalWidth = productWidth * visibleProducts + gap * (visibleProducts + 1);
+        scrollAmount = Math.min(scrollAmount, (totalGroups + 1) * totalWidth);
+        productContainer.style.transform = `translateX(-${scrollAmount}px)`;
+    }
 
-      if (scrollAmount === 0) {
-          prevBtn.disabled = true;
-      }
+    function updateButtons() {
+        prevBtn.disabled = scrollAmount <= 0;
+        nextBtn.disabled = scrollAmount >= (totalGroups + 1 ) * (desktopProductWidth + gap);
+    }
 
-      nextBtn.disabled = false;
-  });
+    function handleResize() {
+        updateVisibleProducts();
+        updateScrollAmount();
+        updateButtons();
+    }
+
+    const totalProducts = document.querySelectorAll('.cardProduct1').length;
+    const totalGroups = Math.ceil(totalProducts / visibleProducts);
+
+    window.addEventListener('resize', () => {
+        handleResize();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        let productWidth;
+
+        if (visibleProducts === 3) {
+            productWidth = desktopProductWidth;
+        } else if (visibleProducts === 2) {
+            productWidth = tabletProductWidth;
+        } else {
+            productWidth = phoneProductWidth;
+        }
+
+        scrollAmount += (productWidth + gap) * visibleProducts;
+        updateScrollAmount();
+        updateButtons();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        let productWidth;
+
+        if (visibleProducts === 3) {
+            productWidth = desktopProductWidth;
+        } else if (visibleProducts === 2) {
+            productWidth = tabletProductWidth;
+        } else {
+            productWidth = phoneProductWidth;
+        }
+
+        scrollAmount -= (productWidth + gap) * visibleProducts;
+        updateScrollAmount();
+        updateButtons();
+    });
+
+    handleResize();
 });
-
-  
